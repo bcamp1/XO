20f8f59576465340f3e663a19d26430bee520cfc
bcamp1 <bransoncamp@outlook.com>
2018-01-21 08:33:32 -0700

    Added more comments

--- xo.py
+++ xo.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-#Project XO by Branson Camp, 12-12-2017
+"""Project XO by Branson Camp, 11-01-2017"""
 
 from PIL import Image
 import SimpleNet as net
 import os
 from colorama import Fore, Back, Style
 
+# Gets current directory
 os.chdir(os.path.dirname(__file__))
 dir = os.getcwd()
 
+# Sets console color codes
 red = Fore.RED
-green = Fore.GREEN;
-blue = Fore.BLUE;
-yellow = Fore.YELLOW;
-cyan = Fore.CYAN;
-magenta = Fore.MAGENTA;
-white = Fore.WHITE;
+green = Fore.GREEN
+blue = Fore.BLUE
+yellow = Fore.YELLOW
+cyan = Fore.CYAN
+magenta = Fore.MAGENTA
+white = Fore.WHITE
 
+# Puts test.png into an array of pixel values for testing
 def t():
     pic = []
     im = Image.open(dir + "/test.png")
@@ -31,46 +34,55 @@ pic = []
 data = []
 answer = []
 
+# All of the training images
 X = ["x0.png", "x1.png", "x2.png", "x3.png", "x4.png", "x5.png", "x6.png", "x7.png", "x8.png", "x9.png", "x10.png", "x11.png", "x12.png", "x13.png", "x14.png", ]
 O = ["o0.png", "o1.png", "o2.png", "o3.png", "o4.png", "o5.png", "o6.png", "o7.png", "o8.png", "o9.png", "o10.png", "o11.png", "o12.png", "o13.png", "o14.png", ]
 
+# For every X image
 for fname in X:
     im = Image.open(dir + "/TrainingImages/" + fname)
     pix = im.load()
 
+    # Append each pixel color for each 28x28 image
     for y in range(28):
         for x in range(28):
             pic.append(pix[x, y][0] / 255)
 
+    # Append all images to data
     data.append(pic)
     answer.append(1)
     pic = []
 
+# For every O image
 for fname in O:
     im = Image.open(dir + "/TrainingImages/" + fname)
     pix = im.load()
 
+    # Append each pixel color for each 28x28 image
     for y in range(28):
         for x in range(28):
             pic.append(pix[x, y][0] / 255)
 
+    # Append all images to data
     data.append(pic)
     answer.append(0)
     pic = []
+
+# Hand off the image data to SimpleNet
 net.data = data
 net.answer = answer
-net.train(20000, 1)
+net.train(20000, 1) # Begins the training process
 
 #input
-choice = "";
+choice = ""
 while choice != "q":
-    print(green + "[T] " + white + " Test with test.png");
-    print(green + "[R] " + white + " Redo Traning Process");
-    print(green + "[C] " + white + " Custom Train");
-    print(green + "[E] " + white + " Edit Test Image");
-    print(green + "[Q] " + white + " Quit");
-    choice = input(green + "\n>> " + Fore.WHITE).lower();
-    print();
+    print(green + "[T] " + white + " Test with test.png")
+    print(green + "[R] " + white + " Redo Traning Process")
+    print(green + "[C] " + white + " Custom Train")
+    print(green + "[E] " + white + " Edit Test Image")
+    print(green + "[Q] " + white + " Quit")
+    choice = input(green + "\n>> " + Fore.WHITE).lower()
+    print()
 
     if choice == "t":
         t()
